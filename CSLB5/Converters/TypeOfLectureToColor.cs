6562d7c2e8d89d2d002094eae316f663f2b34adf
Arina Dybal <127520119+cianami@users.noreply.github.com>
2023-04-14 23:22:27 +0300

    BrushConverter

---
+++ CSLB5/Converters/TypeOfLectureToColor.cs
@@ -0,0 +1,27 @@
+﻿using MVVM.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using static CSLB5.Resources.Extensions;
+
+namespace CSLB5.Converters
+{
+    internal class TypeOfLectureToColor : ConverterBase<TypeOfLectureToColor>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string) return new SolidColorBrush(Colors.Firebrick);
+            if (value == "л") return NumberBrushes[Resources.TypeOfLecturers.Lecture];
+            if (value == "лб") return NumberBrushes[Resources.TypeOfLecturers.LaboratoryWork];
+            if (value == "конс") return NumberBrushes[Resources.TypeOfLecturers.Consultation];
+            if (value == "п") return NumberBrushes[Resources.TypeOfLecturers.Practice];
+            if (value == "п.экз") return NumberBrushes[Resources.TypeOfLecturers.ExamRetake];
+            if (value == "экз") return NumberBrushes[Resources.TypeOfLecturers.Exam];
+            return new SolidColorBrush(Colors.Firebrick);
+        }
+    }
+}
---
+++ CSLB5/Resources/TypesOfLecturers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLB5.Resources
+{
+
+    public enum TypeOfLecturers
+    {
+        Consultation,
+        Lecture,
+        LaboratoryWork,
+        Practice,
+        ExamRetake,
+        Exam
+    }
+}
