8fc19212e75cdebde3de85384bf3a0bd99f17a47
Arina Dybal <127520119+cianami@users.noreply.github.com>
2023-05-24 17:39:06 +0300

    prism structure

---
+++ SuperCHSU.MainModule/MainModule.cs
@@ -0,0 +1,19 @@
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
+
+namespace SuperCHSU.MainModule
+{
+    public class MainModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+           
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
